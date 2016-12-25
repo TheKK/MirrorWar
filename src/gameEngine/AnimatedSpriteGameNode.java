29dafc80c92c99c6d25a986a8befc81d9082231b
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2016-12-25 23:20:03 +0800

    Now AnimatedSprite can load JSON aseprite created

--- src/gameEngine/AnimatedSpriteGameNode.java
+++ src/gameEngine/AnimatedSpriteGameNode.java
@@ -1,41 +1,99 @@
 package gameEngine;
 
 import java.awt.geom.Rectangle2D;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Optional;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 public class AnimatedSpriteGameNode extends GameNode {
+	static class Frame {
+		Rectangle2D.Double clilp;
+		long duration;
+	}
+
 	Image image;
 
-	ArrayList<Long> timeSegments = new ArrayList<>();
-	ArrayList<Rectangle2D.Double> frameClips = new ArrayList<>();
+	ArrayList<Frame> frameClips = new ArrayList<>();
 
-	long currentTimeElapse = 0;
+	long frameDurationRemains = 0;
 	Integer currentFrame = 0;
 
+	private int loopCountRemains = -1;
+
 	public boolean autoPlayed = true;
-	public Optional<Rectangle2D.Double> clip = Optional.empty();
+
+	// TODO It should be possible to load image from path which provided by JSON file
+	public static AnimatedSpriteGameNode fromAsepriteJson(Image image, String filePath) throws FileNotFoundException {
+		JsonParser parser = new JsonParser();
+		JsonElement element = parser.parse(new FileReader(filePath));
+
+		JsonObject aseprite = element.getAsJsonObject();
+		JsonArray asepriteFrames = aseprite.get("frames").getAsJsonArray();
+
+		AnimatedSpriteGameNode result = new AnimatedSpriteGameNode(image, 0, 0);
+
+		for (JsonElement asepriteFrame: asepriteFrames) {
+			JsonObject frame = asepriteFrame.getAsJsonObject()
+					.get("frame").getAsJsonObject();
+
+			int x = frame.get("x").getAsInt();
+			int y = frame.get("y").getAsInt();
+			int width = frame.get("w").getAsInt();
+			int height = frame.get("h").getAsInt();
+
+			int duration = asepriteFrame.getAsJsonObject()
+					.get("duration").getAsInt();
+
+			result.addFrame(new Rectangle2D.Double(x, y, width, height), duration);
+		}
+
+		if (!result.frameClips.isEmpty()) {
+			Rectangle2D.Double clip = result.frameClips.get(0).clilp;
+
+			result.geometry.width = clip.width;
+			result.geometry.height = clip.height;
+		}
+
+		return result;
+	}
 
 	public AnimatedSpriteGameNode(Image image, int width, int height) {
 		this.image = image;
-		this.timeSegments.add(0L);
 
 		this.geometry.width = width;
 		this.geometry.height = height;
 	}
 
-	public void addFrame(Rectangle2D.Double frameClip, long displayPeriod) {
-		Long lastTimeSegment = timeSegments.get(timeSegments.size() - 1);
+	public void play(int loops) {
+		loopCountRemains = loops;
+		autoPlayed = true;
+	}
 
-		frameClips.add(frameClip);
-		timeSegments.add(lastTimeSegment + displayPeriod);
+	public void playFromStart(int loops) {
+		play(loops);
+
+		currentFrame = 0;
+	}
+
+	public void addFrame(Rectangle2D.Double frameClip, long displayPeriod) {
+		Frame newFrame = new Frame();
+
+		newFrame.clilp = frameClip;
+		newFrame.duration = displayPeriod;
+
+		frameClips.add(newFrame);
 	}
 
 	public void setFrameIndex(int index) throws Exception {
-		if (index <= timeSegments.size()) {
+		if (index <= frameClips.size()) {
 			throw new Exception("frame index out of bound!");
 		}
 
@@ -46,48 +104,31 @@ public class AnimatedSpriteGameNode extends GameNode {
 		currentFrame += 1;
 		if (currentFrame >= frameClips.size()) {
 			currentFrame = 0;
+
+			if (loopCountRemains > 0) {
+				loopCountRemains -= 1;
+			}
 		}
 
-		currentTimeElapse = timeSegments.get(currentFrame);
-	}
-
-	public void previousFrame() {
-		currentFrame -= 1;
-		if (currentFrame <= 0) {
-			currentFrame = frameClips.size() - 1;
-		}
-
-		currentTimeElapse = timeSegments.get(currentFrame);
+		frameDurationRemains += frameClips.get(currentFrame).duration;
 	}
 
 	@Override
 	public void update(long elapse) {
-		// FIXME This is just a stub, remove all of this when Animation was done
-		if (!autoPlayed) {
+		if (!autoPlayed || (loopCountRemains == 0)) {
 			return;
 		}
 
-		currentTimeElapse += elapse;
+		frameDurationRemains -= elapse;
 
-		long timeOffset = currentTimeElapse - timeSegments.get(currentFrame + 1);
-
-		if (timeOffset >= 0) {
+		while (frameDurationRemains < 0) {
 			nextFrame();
-			currentTimeElapse += timeOffset;
 		}
 	}
 
 	@Override
 	public void render(GraphicsContext gc) {
-		Rectangle2D clipRect = frameClips.get(currentFrame);
-
-		if (clip.isPresent()) {
-			Rectangle2D.Double userClip = (Rectangle2D.Double) clip.get().clone();
-			userClip.x += clipRect.getX();
-			userClip.y += clipRect.getY();
-
-			clipRect = clipRect.createIntersection(userClip);
-		}
+		Rectangle2D clipRect = frameClips.get(currentFrame).clilp;
 
 		gc.drawImage(
 				image,
